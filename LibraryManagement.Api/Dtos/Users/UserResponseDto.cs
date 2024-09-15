3f0183158519e5a87ef705a6e1666d2c7e2cd5b4
Alexandre <alexandrecarlos2@gmai.com>
2024-09-15 16:51:53 -0300

    adding rules when deleting users and books

--- LibraryManagement.Api/Dtos/Users/UserResponseDto.cs
+++ LibraryManagement.Api/Dtos/Users/UserResponseDto.cs
@@ -10,19 +10,11 @@ namespace LibraryManagement.Api.Dtos.Users
             Email = email;   
         }
 
-        public UserResponseDto(string name, string email, List<Book> books)
-        {
-            Name = name;
-            Email = email;
-            Books = books;
-        }
-
         public string Name { get; set; }
         public string Email { get; set; }
 
-        public List<Book> Books { get; set; }
 
-        public static UserResponseDto FromEntity(User user) => new UserResponseDto(user.Name, user.Email, user.Books);
+        public static UserResponseDto FromEntity(User user) => new UserResponseDto(user.Name, user.Email);
 
     }
 }
--- LibraryManagement.Api/Entities/User.cs
+++ LibraryManagement.Api/Entities/User.cs
@@ -2,17 +2,15 @@
 {
     public class User : BaseEntity
     {
-        public User(string name, string email, string password) : base()
+        public User(string name, string email) : base()
         {
             Name = name;
             Email = email;
-            Password = password;
             Loans = [];
         }
 
         public string Name { get; private set; }
         public string Email { get; private set; }
-        public string Password { get; private set; }
         public List<Loan> Loans { get; private set; }
 
         public void SetName(string name) => Name = name;
